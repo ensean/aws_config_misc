35ca7e0c88a24f316d8ef1c04064e87901cac3a5
liyxmse <liyxmse@amazon.com>
2022-01-10 18:24:15 +0800

    more image content type

--- s3/scripts/change_obj_content_type.py
+++ s3/scripts/change_obj_content_type.py
@@ -5,6 +5,7 @@ import boto3
 def lambda_handler(event, context):
 
     obj_key = event["tasks"][0]["s3Key"]
+    obj_ext = obj_key.split('.')[-1]
     bucket_arn = event["tasks"][0]["s3BucketArn"]
     bucket_name = bucket_arn.split(":")[-1]
     task_id = event["tasks"][0]["taskId"]
@@ -17,7 +18,7 @@ def lambda_handler(event, context):
         CopySource={"Bucket": bucket_name, "Key": obj_key},
         Metadata={"src": "dth"},
         MetadataDirective="REPLACE",
-        ContentType="image/png",
+        ContentType=get_content_type(obj_ext),
     )
 
     return {
@@ -28,3 +29,13 @@ def lambda_handler(event, context):
             {"taskId": task_id, "resultCode": "Succeeded", "resultString": "Great"}
         ],
     }
+
+
+def get_content_type(ext):
+    type_dict = {
+        'jpg': 'image/jpeg',
+        'png': 'image/png',
+        'gif': 'image/gif',
+        'jpeg': 'image/jpeg'
+    }
+    return type_dict.get(ext, 'binary/octet-stream')
