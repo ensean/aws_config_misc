5f9049e2a8ed6e21795bbc86ea5a242d3f143706
liyx <liyxzsu@gmail.com>
2025-03-11 09:03:07 +0800

    deepseek r1 converse api demo

---
+++ bedrock/deepseek_r1_converse.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
+
+# Create a Bedrock Runtime client in the AWS Region you want to use.
+client = boto3.client("bedrock-runtime", region_name="us-west-2")
+
+# Set the model ID, e.g., Llama 3 8b Instruct.
+model_id = "us.deepseek.r1-v1:0"
+
+# Start a conversation with the user message.
+user_message = "why is the sky blue?"
+conversation = [
+    {
+        "role": "user",
+        "content": [{"text": user_message}],
+    }
+]
+
+try:
+    # Send the message to the model, using a basic inference configuration.
+    response = client.converse(
+        modelId=model_id,
+        messages=conversation,
+        inferenceConfig={"maxTokens": 2000, "temperature": 0.6, "topP": 0.9},
+    )
+
+    # Extract and print the response text.
+    response_text = response["output"]["message"]["content"][0]["text"]
+    print(response_text)
+
+except (ClientError, Exception) as e:
+    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+    exit(1)
